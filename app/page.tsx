478d9fa30f05bd13f104a3cd36b13e42980a93d7
Daniel Adetaiwo <danieladetaiwoeniola@gmail.com>
2025-06-07 22:44:25 +0100

    ZEEKR

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 "use client"
-import React, { useEffect } from 'react'
+import React from 'react'
 import Home from './utils/home'
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
